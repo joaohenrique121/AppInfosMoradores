7efab108ad5d6ee2c890a3b6a33bb9738d552f22
joaohenrique121 <simionatojoaohenrique@gmail.com>
2025-05-24 21:16:09 -0300

    first commit

---
+++ src/main/java/com/mycompany/appmoradorescondominio/AppMoradoresCondominio.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.appmoradorescondominio;
+
+/**
+ *
+ * @author 9gxbv
+ */
+public class AppMoradoresCondominio {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
